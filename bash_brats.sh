f6c40ddca3767a45a3a32a5c74aa96bdb48bbf61
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-01-21 01:52:52 -0500

    bash script paths updated

--- bash_brats.sh
+++ bash_brats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_brats_idwt_dec
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/SegMamba/results/brats_1111_idwt_dec.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/SegMamba/results/brats_1111_idwt_dec.%J.err
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/brats_1111_idwt_dec.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/brats_1111_idwt_dec.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
@@ -13,7 +13,7 @@
 module load conda
 conda activate waveformer
 
-cd /blue/r.forghani/mdmahfuzalhasan/scripts/SegMamba
+cd /blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats
 
 # Execute the Python script
 srun python 3_train.py

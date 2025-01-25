e6bbcfa583fe49445aa72fc5bf675700b33dda12
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-01-25 18:10:52 -0500

    updated file name in bash file

--- bash_brats_evaluate.sh
+++ bash_brats_evaluate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=eval_brats_idwt_dec
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_up_in_block.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_up_in_block.%J.err
+#SBATCH --job-name=eval_brats_idwt_dec_wd_1e-5
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_idwt_dec_wd_1e-5.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_idwt_dec_wd_1e-5.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128GB
 #SBATCH --partition=hpg-ai
 #SBATCH --gpus=a100:1
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 
 module load conda
 conda activate waveformer

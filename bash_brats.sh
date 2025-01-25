4026fce4b1604247f05084a5b49e404314ae40a7
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-01-25 13:38:13 -0500

    bash script updated

--- bash_brats.sh
+++ bash_brats.sh
@@ -4,11 +4,11 @@
 #SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/brats_1111_idwt_wd_1e-5.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=256GB
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=512GB
 #SBATCH --partition=hpg-ai
-#SBATCH --gpus=a100:2
-#SBATCH --time=48:00:00
+#SBATCH --gpus=a100:4
+#SBATCH --time=30:00:00
 
 module load conda
 conda activate waveformer

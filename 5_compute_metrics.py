3c4fcbeabf2df18378757ea771f27cf79d2373d3
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-01-29 16:40:30 -0500

    evaluating model trained with dice loss

--- 5_compute_metrics.py
+++ 5_compute_metrics.py
@@ -18,7 +18,8 @@ parser.add_argument("--pred_name", required=True, type=str)
 
 results_root = "prediction_results"
 args = parser.parse_args()
-model_name = "model_upsample_inside_wd_1e-5_4_gpu"
+# model_name = "model_upsample_inside_wd_1e-5_4_gpu"
+model_name = "model_loss_dice_opt_adamw"
 pred_name = args.pred_name
 
 def cal_metric(gt, pred, voxel_spacing):

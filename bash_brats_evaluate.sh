4a2ad8d805f83c99ad9c565ed08adfde1e15e4fa
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2025-02-07 22:29:21 -0500

    bash file names updated

--- bash_brats_evaluate.sh
+++ bash_brats_evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=eval_brats_segmamba_setup
-#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_segmamba_setup.%J.out
-#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_segmamba_setup.%J.err
+#SBATCH --job-name=eval_brats_ablation_pooling
+#SBATCH --output=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_ablation_pooling.%J.out
+#SBATCH --error=/blue/r.forghani/mdmahfuzalhasan/scripts/waveformer-brats/results/eval_brats_ablation_pooling.%J.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
